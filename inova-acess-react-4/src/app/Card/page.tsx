6bdb94fab638aabc2ba5fc1aa59bca65439dff86
= <=>
2024-03-22 09:51:07 -0300

    aperfeicoamentoHeaderFooter

--- inova-acess-react-4/src/app/Card/page.tsx
+++ inova-acess-react-4/src/app/Card/page.tsx
@@ -5,6 +5,9 @@ import Sul from '../img/sulamericalogo.png'
 import Caloi from '../img/caloilogo.png'
 import Alelo from '../img/alelologo.png'
 import Sumup from '../img/sumuplogo.png'
+import Vivo from '../img/vivo.png'
+import Fedex from '../img/fedex.png'
+import Gympass from '../img/gympass.png'
 
 const Card = () =>{
     return(
@@ -12,10 +15,13 @@ const Card = () =>{
             <main>
                 <div className="content2">
                     <ul>
-                        <Image src={Sul} alt="Landscape picture" width={300} height={200} className='card efeito'/>
-                        <Image src={Caloi} alt="Landscape picture" width={300} height={200} className='card efeito' />
-                        <Image src={Alelo} alt="Landscape picture" width={300} height={200} className='card efeito'/>
-                        <Image src={Sumup} alt="Landscape picture" width={300} height={200} className='card efeito'/>
+                        <Image src={Sul} alt="LogoSulamerica" width={300} height={200} className='card efeito'/>
+                        <Image src={Caloi} alt="LogoCaloi picture" width={300} height={200} className='card efeito' />
+                        <Image src={Alelo} alt="LogoAlelo picture" width={300} height={200} className='card efeito'/>
+                        <Image src={Sumup} alt="LogoSumup" width={300} height={200} className='card efeito'/>
+                        <Image src={Vivo} alt="LogoVivo" width={300} height={200} className='card efeito'/>
+                        <Image src={Fedex} alt="LogoFedex" width={300} height={200} className='card efeito'/>
+                        <Image src={Gympass} alt="LogoGympass" width={300} height={200} className='card efeito'/>
                     </ul>
                 </div>
             </main>
