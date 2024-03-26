c27c4127dbc2c6a88bb1275e416f19ad7391891a
LuAlcantara <oalcantara.lucas@gmail.com>
2024-03-26 10:42:41 -0300

    aperfeicoamentoV13

--- inova-acess-react-4/src/app/Empresa/page.tsx
+++ inova-acess-react-4/src/app/Empresa/page.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
+import "./empresa.css";
 import Footer from "../Footer/page";
 import Header from "../Header/page";
+import Sobre from "../img/sobre.png"
 
 const Empresa = () =>{
     return(
@@ -7,7 +10,23 @@ const Empresa = () =>{
             <Header></Header>
             <main>
                 <div className="content-empresa">
-                    <h1>EMPRESA</h1>
+                    <h1>O que é a Salesforce?</h1>
+                    <p>A Salesforce é uma empresa de softwares que foca na solução de gerenciamento de relacionamento para aproximar empresas e pessoas. 
+                        É uma plataforma de CRM integrada que oferece a todos os departamentos uma visão única e compartilhada de cada cliente.
+                    </p>
+                <div className="container-imagem">
+                    <Image src={Sobre} alt="Sobre Imagem" className="sobre-imagem" />
+                </div>
+                <h2>Juntos, conectamos as empresas aos seus clientes</h2>
+                <p>Por que usar Salesforce?</p>
+                <ul className="lista-pqs">
+                    <li className="lista-item">Atraia mais compradores com uma estratégia de marketing personalizada.</li>
+                    <li className="lista-item">Conquiste mais clientes conhecendo suas necessidades e preocupações.</li>
+                    <li className="lista-item">Atenda às expectativas dos seus clientes oferecendo experiências incríveis de compra.</li>
+                    <li className="lista-item">Responda rapidamente aos problemas de suporte ao cliente em qualquer canal.</li>
+                    <li className="lista-item">Automatize tarefas demoradas criando aplicativos personalizados.</li>
+                </ul>
+                
                 </div>
             </main>
             <section id="footer">
--- inova-acess-react-4/src/app/Header/page.tsx
+++ inova-acess-react-4/src/app/Header/page.tsx
@@ -12,9 +12,6 @@ const Header = () =>{
         <a href="/"><Image src={Logo} alt="Landscape picture" width={150} height={100} /></a>
         <nav>
             <ul>
-                {/*<li><a href="/Produto">Produtos</a></li>*/}
-                {/*<li><a href="/Industrias">Indústrias</a></li>*/}
-                {/*<li><a href="/Aprendizado">Aprendizado & Recursos</a></li>*/}
                 <li><a href="/"><Image src={BtnHome} alt="Home" width={30} height={30} className='posicao-home'/></a></li>
                 <li><a href="/Empresa">Empresa</a></li>
                 <li><a href="/Suporte">Suporte</a></li>
