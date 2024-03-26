ccfb2a4265570efa3749d1aac08fc71cc3917b36
LuAlcantara <oalcantara.lucas@gmail.com>
2024-03-26 12:22:53 -0300

    aperfeicoamentoV14

--- inova-acess-react-4/src/app/Empresa/page.tsx
+++ inova-acess-react-4/src/app/Empresa/page.tsx
@@ -14,18 +14,19 @@ const Empresa = () =>{
                     <p>A Salesforce é uma empresa de softwares que foca na solução de gerenciamento de relacionamento para aproximar empresas e pessoas. 
                         É uma plataforma de CRM integrada que oferece a todos os departamentos uma visão única e compartilhada de cada cliente.
                     </p>
-                <div className="container-imagem">
-                    <Image src={Sobre} alt="Sobre Imagem" className="sobre-imagem" />
-                </div>
-                <h2>Juntos, conectamos as empresas aos seus clientes</h2>
-                <p>Por que usar Salesforce?</p>
-                <ul className="lista-pqs">
-                    <li className="lista-item">Atraia mais compradores com uma estratégia de marketing personalizada.</li>
-                    <li className="lista-item">Conquiste mais clientes conhecendo suas necessidades e preocupações.</li>
-                    <li className="lista-item">Atenda às expectativas dos seus clientes oferecendo experiências incríveis de compra.</li>
-                    <li className="lista-item">Responda rapidamente aos problemas de suporte ao cliente em qualquer canal.</li>
-                    <li className="lista-item">Automatize tarefas demoradas criando aplicativos personalizados.</li>
-                </ul>
+                    <div className="container-imagem">
+                        <Image src={Sobre} alt="Sobre Imagem" className="sobre-imagem" />
+                    </div>
+
+                    <h2>Juntos, conectamos as empresas aos seus clientes</h2>
+                    <h3>Por que usar Salesforce?</h3>
+                    <ul className="lista-pqs">
+                        <li className="lista-item">Atraia mais compradores com uma estratégia de marketing personalizada.</li>
+                        <li className="lista-item">Conquiste mais clientes conhecendo suas necessidades e preocupações.</li>
+                        <li className="lista-item">Atenda às expectativas dos seus clientes oferecendo experiências incríveis de compra.</li>
+                        <li className="lista-item">Responda rapidamente aos problemas de suporte ao cliente em qualquer canal.</li>
+                        <li className="lista-item">Automatize tarefas demoradas criando aplicativos personalizados.</li>
+                    </ul>
                 
                 </div>
             </main>
