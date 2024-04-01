f05d9abe7e1f4f628e0af7698d18868ee859f032
renan-b-eth <renanacademic21@gmail.com>
2024-04-01 08:38:35 -0300

    V21

--- inova-acess-react-4/src/app/CameraMouse/page.tsx
+++ inova-acess-react-4/src/app/CameraMouse/page.tsx
@@ -1,6 +1,8 @@
 import Footer from "../Footer/page";
 import Header from "../Header/page";
 import './cameramouse.css'
+import imagemcamera from '../img/cameramouse.png'
+import Image from 'next/image';
 
 const CameraMouse = () =>{
     return(
@@ -9,6 +11,14 @@ const CameraMouse = () =>{
             <main>
                 <div className="content-camera-mouse">
                     <h1>Camera Mouse</h1>
+                    <div className="content-camera-mouse2">
+                        <p>
+                        Implantação de um sistema, onde um usuário não precisara usar constantemente o mouse. Através de uma câmera (webcam), ela determinará um ponto focal em seu rosto, para que assim seja feito o movimento do ponteiro do mouse no computador.
+                        </p>
+                    </div>
+                    <div className="content-camera-mouse3">
+                    <Image src={imagemcamera} alt="imagemCamera" width={550} height={350} className=''/>
+                    </div>
                 </div>
             </main>
             <section id="footer">
--- inova-acess-react-4/src/app/TecladoVirtual/page.tsx
+++ inova-acess-react-4/src/app/TecladoVirtual/page.tsx
@@ -1,14 +1,24 @@
 import Footer from "../Footer/page";
 import Header from "../Header/page";
 import './tecladoVirtual.css'
+import imagemteclado from '../img/tecladovirtual.png'
+import Image from 'next/image';
 
 const TecladoVirtual = () =>{
     return(
         <>
             <Header></Header>
             <main>
-                <div className="content-teclado-virtual">
+            <div className="content-teclado-virtual">
                     <h1>Teclado Virtual</h1>
+                    <div className="content-teclado-virtual2">
+                        <p>
+                        O Teclado Virtual é uma ferramenta que permite aos usuários inserir texto e comandos usando uma interface visual na tela, em vez de um teclado físico, tornando a interação com o site mais acessível, especialmente para pessoas com limitações de mobilidade.
+                        </p>
+                    </div>
+                    <div className="content-teclado-virtual3">
+                    <Image src={imagemteclado} alt="imagemCamera" width={550} height={350} className=''/>
+                    </div>
                 </div>
             </main>
             <section id="footer">
